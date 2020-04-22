294e14f949ec7a642602bcea01cfab293df3a826
burakalp <burakalp@klonbits.com>
2020-04-22 16:04:41 +0300

    last changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 var slideSpaceMobile = 8;
 var slideSpaceTablet = 10;
 var slideSpaceDesktop = 18;
+var ww = $(window).outerWidth();
 
 var bgColorList = [];
 
@@ -23,38 +24,54 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  $(window).on('resize orientedChanged',function(){
-    $('[class*=-slider]:not([class*=-slider-block])').each(function(){
-        var $thisSwiper = $(this)[0].swiper;
+  $(window).on('resize orientedChanged', function () {
+    $('[class*=-slider]:not([class*=-slider-block])').each(function () {
+      var $thisSwiper = $(this)[0].swiper;
 
-        if($thisSwiper != undefined || $thisSwiper != "") {
-            $thisSwiper.update(true)
-        }
-    })
-  })
+      if ($thisSwiper != undefined || $thisSwiper != null) {
+        $thisSwiper.update(true);
+      }
+    });
+  });
+
+  function setCardBackgrounds(sliderSlide) {
+    var bgListRandom = Math.floor(Math.random() * (bgColorList.length - 1)) + 1;
+    var isSameBg = $(sliderSlide).prev().find('.cards').hasClass(bgColorList[bgListRandom]);
+
+    if (isSameBg) {
+      bgListRandom = Math.floor(Math.random() * (bgColorList.length - 1)) + 1;
+    }
+
+    $(sliderSlide).find('.cards').addClass(bgColorList[bgListRandom]);
+  }
 
   $('.cards-slider').each(function () {
     var $this = $(this);
-    var slideCssClass = 'cards-slide';
+    var isCardsSlider = $this.hasClass('cards-slider--standart');
+    ww = $(window).outerWidth();
 
-    $this
-      .find('.cards-slide')
-      .each(function () {
-        var bgListRandom = Math.floor(Math.random() * (bgColorList.length - 1)) + 1;
-
-        var isSameBg = $(this).prev().find('.cards').hasClass(bgColorList[bgListRandom]);
-
-        if (isSameBg) {
-          bgListRandom = Math.floor(Math.random() * (bgColorList.length - 1)) + 1;
-        }
-
-        $(this).find('.cards').addClass(bgColorList[bgListRandom]);
+    if (!isCardsSlider) {
+      $this.find('.cards-slide').each(function (idx, el) {
+        setCardBackgrounds(el);
       });
+    } else {
+      if (ww < 768) {
+        $this.find('.cards-slide').each(function (idx, el) {
+          setCardBackgrounds(el);
+        });
+      }
+    }
+  });
+
+  $('.cards-slider--standart').each(function () {
+    var $this = $(this);
+    var slideCssClass = 'cards-slide';
+    var cardsSliderStandart = null;
+    ww = $(window).outerWidth();
 
     var config = {
       slidesPerView: 1.25,
       spaceBetween: slideSpaceMobile,
-      loop: true,
       centeredSlides: true,
       containerModifierClass: 'cards-slider--',
       wrapperClass: 'cards-wrapper',
@@ -64,15 +81,99 @@ document.addEventListener('DOMContentLoaded', function () {
       slidePrevClass: slideCssClass + '--prev',
       breakpoints: {
         768: {
-          slidesPerView: 4,
+          slidesPerView: 1,
+          centeredSlides: false,
+          spaceBetween: slideSpaceTablet,
+        },
+        1440: {
+          slidesPerView: 1,
+          centeredSlides: false,
+          spaceBetween: slideSpaceDesktop,
+        },
+      },
+    };
+
+    var configTwo = {
+      slidesPerView: 1,
+      spaceBetween: slideSpaceDesktop,
+      effect: 'fade',
+      containerModifierClass: 'cards-slider--',
+      wrapperClass: 'cards-wrapper',
+      slideClass: slideCssClass,
+      slideActiveClass: slideCssClass + '--active',
+      slideNextClass: slideCssClass + '--next',
+      slidePrevClass: slideCssClass + '--prev',
+    };
+
+    if (ww < 768) {
+      cardsSliderStandart = new Swiper($this, config);
+    } else {
+      cardsSliderStandart = new Swiper($this, configTwo);
+    }
+
+    $(window).on('resize orientedChange', function () {
+      ww = $(window).outerWidth();
+
+      var $cardsWrapper = $this.find('.cards-wrapper');
+      var clonedSlides = $cardsWrapper.children('.cards-slide').clone(true);
+
+      cardsSliderStandart.destroy();
+      $cardsWrapper.empty().append(clonedSlides);
+      $cardsWrapper.attr('style', '');
+
+      if (ww < 768) {
+        cardsSliderStandart = new Swiper($this, config);
+      } else {
+        cardsSliderStandart = new Swiper($this, configTwo);
+      }
+    });
+  });
+
+  $('.cards-slider:not(.cards-slider--standart)').each(function () {
+    var $this = $(this);
+    var slideCssClass = 'cards-slide';
+    var hasPerviewProp = false;
+    var dataSlidePerview = $this.data('slides-perview');
+    var slidesPerViewValues = '';
+    var slidePerviewSM = '';
+    var slidePerviewMD = '';
+    var slidePerviewLG = '';
+    var dataEffect = $this.data('effect');
+
+    if (dataSlidePerview != undefined) {
+      hasPerviewProp = true;
+      slidesPerViewValues = $('[data-slides-perview]').data('slidesPerview').split(',');
+
+      slidesPerViewValues.forEach(function (e, idx) {
+        if (e.includes('sm')) slidesPerviewSM = e.split(':')[1];
+        else if (e.includes('md')) slidesPerviewMD = e.split(':')[1];
+        else slidesPerviewLG = e.split(':')[1];
+      });
+    }
+
+    var config = {
+      slidesPerView: 1.25,
+      spaceBetween: slideSpaceMobile,
+      // loop: true,
+      centeredSlides: true,
+      containerModifierClass: 'cards-slider--',
+      wrapperClass: 'cards-wrapper',
+      slideClass: slideCssClass,
+      slideActiveClass: slideCssClass + '--active',
+      slideNextClass: slideCssClass + '--next',
+      slidePrevClass: slideCssClass + '--prev',
+      breakpoints: {
+        768: {
+          slidesPerView: hasPerviewProp ? slidesPerviewMD : 4,
           centeredSlides: false,
           spaceBetween: slideSpaceTablet,
           navigation: {
-            nextEl: '.slider-controls--right',
+            nextEl: $this.parent().find('.slider-controls--right'),
           },
         },
         1440: {
-          slidesPerView: 5,
+          slidesPerView: hasPerviewProp ? slidesPerviewLG : 5,
+          centeredSlides: false,
           spaceBetween: slideSpaceDesktop,
           navigation: {
             nextEl: $this.parent().find('.slider-controls--right'),
@@ -84,7 +185,14 @@ document.addEventListener('DOMContentLoaded', function () {
     var cardsSlider = new Swiper($this, config);
   });
 
-  $('.cards.standart').each(function(){
-    $(this).find('.description').height() >= 100 ? $(this).find('.description').addClass("with-three-dots") : ""
-  })
+  $('.cards.standart').each(function () {
+    $(this).find('.description').height() >= 100 ? $(this).find('.description').addClass('with-three-dots') : '';
+  });
+
+  $('.header-main-menu').each(function () {
+    $('.header-list-item').on('click', function () {
+      $('.header-list-item').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
 });
